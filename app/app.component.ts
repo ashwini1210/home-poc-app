5003f6972f17b9bef3ac2b9923030b8dc1820834
Ashwini Pathare <ashwini923516@gmail.com>
2017-07-16 14:30:18 +0530

    created Author

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,13 @@
 import {Component} from 'angular2/core';
 import {CourseComponent} from './course.component'
+import {AuthorComponent} from './author.component'
 
 @Component({
     selector: 'home-poc-app',
     template: `<h1>Home POC Application</h1>
-               <courses></courses>`,
-  directives: [CourseComponent]
+               <courses></courses> 
+               <author></author>
+               `,
+  directives: [CourseComponent,AuthorComponent]
 })
 export class AppComponent { }
---
+++ app/author.component.ts
@@ -0,0 +1,22 @@
+import {Component} from "angular2/core"
+import {AuthorService} from "./author.service"
+
+@Component({
+    selector:"author",
+    template:`
+    <h1>Author</h1>
+    <h2>{{title}}</h2>
+    <ul>
+        <li *ngFor="#author of authors">{{author}}</li>
+    </ul>
+    `,
+    providers:[AuthorService]
+})
+export class AuthorComponent{
+    title= "This is list of author"
+    authors;
+
+    constructor(authorService : AuthorService){
+        this.authors = authorService.getAuthor()
+    }
+}
